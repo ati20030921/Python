31624729328ac042bba7f4385fc73c7bf521ee45
attila20030 <152779773+attila20030@users.noreply.github.com>
2024-02-26 11:23:05 +0100

    helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+def main(page: ft.Page):
+
+
+    page.add(
+        ft.Text(value="Hello World!")
+    )
+    page.update()
+
+
+if __name__=='__main__':
+    ft.app(target=main)
