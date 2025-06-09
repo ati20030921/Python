c0d83175d7ba16bacc1cd257c556b1ba91e17930
Attila <szt3daratt@vasvari.org>
2025-06-09 19:45:06 +0200

    four functions was created

---
+++ tictactoe.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import random
+
+# tkinter: For displaying GUI interfaces
+# four functions
+
+def next_turn():
+    pass
+
+def check_winner():
+    pass
+
+def empty_spaces():
+    pass
+
+def new_games():
+    pass
