6672f0b9ffb00d521613be926ca08861fd8186d4
Attila <szt3daratt@vasvari.org>
2025-06-10 00:15:15 +0200

    Change the colors and messages, write a button for another game mode(5x5)

--- tictactoe.py
+++ tictactoe.py
@@ -13,10 +13,10 @@ def next_turn(row, column):
 
             if check_winner() is False:
                 player = players[1]
-                label.config(text=(players[1]+" turn"))
+                label.config(text=(players[1]+" new turn"))
 
             elif check_winner() is True:
-                label.config(text=(players[0]+" wins"))
+                label.config(text=(players[0]+" Fucking cunt"))
 
             elif check_winner() == "Tie":
                 label.config(text="Tie!")
@@ -39,47 +39,91 @@ def check_winner():
 
     for row in range(3):
         if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
-            buttons[row][0].config(bg="green")
-            buttons[row][1].config(bg="green")
-            buttons[row][2].config(bg="green")
+            buttons[row][0].config(bg="lightblue")
+            buttons[row][1].config(bg="lightblue")
+            buttons[row][2].config(bg="lightblue")
             return True
 
     for column in range(3):
         if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
-            buttons[0][column].config(bg="green")
-            buttons[1][column].config(bg="green")
-            buttons[2][column].config(bg="green")
+            buttons[0][column].config(bg="lightblue")
+            buttons[1][column].config(bg="lightblue")
+            buttons[2][column].config(bg="lightblue")
             return True
 
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
-        buttons[0][0].config(bg="green")
-        buttons[1][1].config(bg="green")
-        buttons[2][2].config(bg="green")
+        buttons[0][0].config(bg="lightblue")
+        buttons[1][1].config(bg="lightblue")
+        buttons[2][2].config(bg="lightblue")
         return True
 
     elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
-        buttons[0][2].config(bg="green")
-        buttons[1][1].config(bg="green")
-        buttons[2][0].config(bg="green")
+        buttons[0][2].config(bg="lightblue")
+        buttons[1][1].config(bg="lightblue")
+        buttons[2][0].config(bg="lightblue")
         return True
 
     elif empty_spaces() is False:
 
         for row in range(3):
             for column in range(3):
-                buttons[row][column].config(bg="yellow")
+                buttons[row][column].config(bg="red")
         return "Tie"
-
     else:
         return False
 
 
-
 def empty_spaces():
-    pass
+
+    spaces = 9
+    for row in range(3):
+        for column in range(3):
+            if buttons[row][column]['text'] != "":
+                spaces -= 1
+
+    if spaces == 0:
+        return False
+    else:
+        return True
 
 def new_game():
-    pass
+
+    global player
+
+    player = random.choice(players)
+
+    label.config(text=player+" turn")
+
+    for row in range(3):
+        for column in range(3):
+            buttons[row][column].config(text="",bg="#4d4dff")
+
+
+window = Tk()
+window.title("Tic-Tac-Tounge")
+players = [":)",":("]
+player = random.choice(players)
+buttons = [[0,0,0],
+           [0,0,0],
+           [0,0,0]]
+
+label = Label(text=player + " turn", font=('consolas',30))
+label.pack(side="top")
+
+reset_button = Button(text="again", font=('consolas',20), command=new_game)
+reset_button.pack(side="bottom")
+
+another_game = Button(text="5x5", font=('consolas',20))
+#Another game mode 5x5
+
+frame = Frame(window)
+frame.pack()
+
+for row in range(3):
+    for column in range(3):
+        buttons[row][column] = Button(frame, text="",font=('consolas',40), width=5, height=2,
+                                      command= lambda row=row, column=column: next_turn(row,column))
+        buttons[row][column].grid(row=row,column=column)
 
 
 window.mainloop()
